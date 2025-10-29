07de67fc276410557d5bd5ad04d78eb4c13929ff
Gur-Sych <dany.gurchin@gmail.com>
2025-10-30 00:57:38 +0300

    Null result problem fix

--- g_equations/__init__.py
+++ g_equations/__init__.py
@@ -1,10 +1,12 @@
 def equ_to_dict(equation_str: str) -> dict:
-    equation_str  = equation_str.replace(' ','')
-    equ_pair      = equation_str.split('=')
+    equation_str = equation_str.replace(' ','')
+    equ_pair     = equation_str.split('=')
     if len(equ_pair) != 2:
         raise Exception("g_equations: Equation must have single-one '=' symbol")
-    equation_dict = dict()
-    equation_str  = equ_pair[0] + '='
+    equation_dict        = dict()
+    equation_dict["res"] = 0.
+    equation_str         = equ_pair[0] + '='
+
     def go_coefficients(k: float) -> None:
         var_indx = 0
         for i in range(0,len(equation_str)):
@@ -17,14 +19,14 @@ def equ_to_dict(equation_str: str) -> dict:
                 raise Exception("g_equations: Loosing statement between math symbols")
             var_name = var_str[-1]
             if var_name.isalpha():
-                var_value = var_str[:-1]
+                var_value     = var_str[:-1]
                 var_value_len = var_str_len - 1
-                local_k = k
+                local_k       = k
             else:
-                var_value = var_str
+                var_value     = var_str
                 var_value_len = var_str_len
-                var_name  = "res"
-                local_k   = -k
+                var_name      = "res"
+                local_k       = -k
             if var_value_len == 0:
                 var_value = 1.0
             elif var_value_len == 1 and var_indx != 0:
@@ -41,6 +43,7 @@ def equ_to_dict(equation_str: str) -> dict:
                 equation_dict[var_name] = var_value * local_k
             if char == '=': break 
             var_indx = i
+            
     go_coefficients(1.)
     equation_str = equ_pair[1] + '='
     go_coefficients(-1.)
